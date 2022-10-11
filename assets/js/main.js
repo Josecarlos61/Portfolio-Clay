f21a47666d0f332bfd8db60a142b4c81ec7a89a0
Josecarlos61 <carlosmelian2013@gmail.com>
2022-10-11 18:19:48 +0100

    terminado el header y el css header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,68 @@
 /*===== SHOW MENU =====*/
+const showMenu = (toggleId, navId) => {
+	const toggle = document.getElementById(toggleId),
+		nav = document.getElementById(navId);
 
+	if (toggle && nav) {
+		toggle.addEventListener("click", () => {
+			nav.classList.toggle("show-menu");
+		});
+	}
+};
+showMenu("nav-toggle", "nav-menu");
 
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll(".nav__link");
 
+function linkActive() {
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkActive));
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+	const scroll = window.pageYOffset;
 
-/*===== CHANGE BACKGROUND HEADER =====*/ 
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+		sectionId = current.getAttribute("id");
 
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+		} else {
+			document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+		}
+	});
+}
+window.addEventListener("scroll", scrollActive);
 
-/*===== SHOW SCROLL TOP =====*/ 
+/*===== CHANGE BACKGROUND HEADER =====*/
+function scrollHeader() {
+	const header = document.getElementById("header");
+	if (this.scrollY >= 200) header.classList.add("scroll-header");
+	else header.classList.remove("scroll-header");
+}
 
+window.addEventListener("scroll", scrollHeader);
 
-/*===== MIXITUP FILTER PORTFOLIO =====*/ 
+/*===== SHOW SCROLL TOP =====*/
+function scrollTop() {
+	const scrollTop = document.getElementById("scroll-top");
+	if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+	else scrollTop.classList.remove("show-scroll");
+}
 
-/* Link active portfolio */ 
+window.addEventListener("scroll", scrollTop);
 
+/*===== MIXITUP FILTER PORTFOLIO =====*/
 
-/*===== SWIPER CAROUSEL =====*/ 
+/* Link active portfolio */
 
+/*===== SWIPER CAROUSEL =====*/
 
-/*===== GSAP ANIMATION =====*/ 
-
-
+/*===== GSAP ANIMATION =====*/
